05c89b1754b0b224a46b3fa1e3a01ab32b2c9352
FabSyntaxDev <fabio.dsm.eng.software@gmail.com>
2026-05-12 11:54:45 -0300

    user inf

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello Mundão!"}
+
+@app.get("/detalhes")
+def read_details():
+    return {
+        "nome" : "Pedro",
+        "sobrenome" : "Silva",
+        "idade" : 30,
+    }
